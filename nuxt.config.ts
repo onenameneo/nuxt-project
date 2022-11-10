7cc1b13e655b034a898f5ecd64bd605193df8b2b
Neo <onenameneo@gmail.com>
2022-11-11 01:13:18 +0800

    temp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,30 @@ export default defineNuxtConfig({
     '@nuxtjs/strapi'
   ],
   strapi: {
+    url: 'http://localhost:3000',
     cookie: {},
     cookieName: 'strapi_jwt'
+  },
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: 'http://localhost:1337/api',
+        changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://localhost:1337/uploads'
+      }
+    }
   },
   vite: {
     server: {
       proxy: {
+        '/api': {
+          target: 'http://localhost:1338',
+          changeOrigin: true
+        },
         '/uploads': {
-          target: 'http://localhost:1337/'
+          target: 'http://localhost:1337/uploads'
         }
       }
     }
