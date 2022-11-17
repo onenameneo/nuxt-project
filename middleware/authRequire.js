3601eb9bcb806dac4ed26020b0cad7167d8cb42d
Neo <neo@wishnt.com>
2022-11-17 08:38:32 +0800

    test

--- middleware/authRequire.js
+++ middleware/authRequire.js
@@ -3,6 +3,7 @@ import { useCookie } from '#app';
 export default defineNuxtRouteMiddleware((to) => {
   const token = useCookie('token')
   if (!token.value) {
+    if (process.client) alert('test: not login')
     return '/login';
   }
 });
